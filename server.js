1095d56bb3811539ae50453ac90061a15574f708
cmdcheshire <39660009+cmdcheshire@users.noreply.github.com>
2025-05-29 14:04:07 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1408,7 +1408,7 @@ async function main() {
                   let pitStatusDataIndex = pitStatusData.findIndex(item => item.carNumber == result.$.Car);
 
                   let newPitStatusDataObject = {
-                    carNumber: result.$.Car_Number,
+                    carNumber: result.$.Car,
                     pitStatus: true,
                     pitStops: result.$.Pit_Number,
                   };
